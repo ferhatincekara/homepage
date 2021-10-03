3d590ab09162895a0320f9338fb6c34f5fee4d94
ferhatincekara <ferhatincekara@gmail.com>
2021-10-03 11:04:13 +0300

    🎨 Add layout to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app';
+import Header from 'components/header';
+import Footer from 'components/footer';
 
 import 'pages/app.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <main id="page" className="main" role="main">
+        <section className="page-section">
+          <Component {...pageProps} />
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 }
 export default MyApp;
