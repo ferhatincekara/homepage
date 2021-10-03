15e328a6459932b493c17cfd64d719cfb1d1c166
ferhatincekara <ferhatincekara@gmail.com>
2021-10-03 13:46:33 +0300

    🐛 Fix head meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import Header from 'components/header';
 import Footer from 'components/footer';
 
 import 'pages/app.scss';
+import SiteConfig from '../site.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>{SiteConfig.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
       <Header />
       <main id="page" className="main" role="main" tabIndex={-1}>
         <section className="page-section">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,11 +13,9 @@ class MyDocument extends NextDocument {
       <Html lang={SiteConfig.lang}>
         <Head>
           {/* base */}
-          <title>{SiteConfig.title}</title>
           <meta charSet="utf-8" />
           <meta name="robots" content="follow, index" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content={SiteConfig.description} />
           <link rel="canonical" href={SiteConfig.siteUrl} />
 
