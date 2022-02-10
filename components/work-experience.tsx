91b2cf2b83c12596e652d132842f7557c8977911
ferhatincekara <ferhatincekara@gmail.com>
2022-02-10 19:37:41 +0300

    ⚡ Update company name

--- components/work-experience.tsx
+++ components/work-experience.tsx
@@ -2,7 +2,7 @@ import { Fragment } from 'react';
 import styles from 'components/work-experience.module.scss';
 
 const WORK = [
-  { position: 'Frontend Developer', company: 'Cousins Factory', date: 'Oct 2021-Current' },
+  { position: 'Frontend Developer', company: 'SHFT', date: 'Oct 2021-Current' },
   { position: 'Frontend Developer', company: 'Ilao', date: 'Jan 2019–Sep 2019' },
   { position: 'IT Intern', company: 'Hyosung Corporation', date: 'Sep 2016–June 2017' },
 ];
