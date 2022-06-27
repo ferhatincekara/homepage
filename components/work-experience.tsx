2fd74bea0756becc2b91a0f5bcf30d39f56c5204
ferhatincekara <ferhatincekara@gmail.com>
2022-06-27 10:57:18 +0300

    Revert "⚡ Add new job experience"
    This reverts commit 5406638a7e41d9daa467b98754bef06c0238cd5b.

--- components/work-experience.tsx
+++ components/work-experience.tsx
@@ -2,8 +2,7 @@ import { Fragment } from 'react';
 import styles from 'components/work-experience.module.scss';
 
 const WORK = [
-  { position: 'Frontend Developer', company: 'Actimi', date: 'June 2022-Current' },
-  { position: 'Frontend Developer', company: 'SHFT', date: 'Oct 2021-June 2022' },
+  { position: 'Frontend Developer', company: 'SHFT', date: 'Oct 2021-Current' },
   { position: 'Frontend Developer', company: 'Ilao', date: 'Jan 2019–Sep 2019' },
   { position: 'IT Intern', company: 'Hyosung Corporation', date: 'Sep 2016–June 2017' },
 ];
